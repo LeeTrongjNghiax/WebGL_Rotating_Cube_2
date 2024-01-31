34a4c1853cb4b854218e5b6e48fb7b1c032ca21d
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2024-01-31 15:40:06 +0700

    set up new engine

--- function.js
+++ function.js
@@ -1,4 +1,38 @@
+/*
+D1 = (n2 - m2)(o3 - m3) - (n3 - m3)(o2 - m2)
+D2 = (n1 - m1)(o3 - m3) - (n3 - m3)(o1 - m1)
+D3 = (n1 - m1)(o2 - m2) - (n2 - m2)(o1 - m1)
+*/
+
 const EPSILON = 0.000001;
+
+function normalize(out, a) {
+    let x = a[0];
+    let y = a[1];
+    let z = a[2];
+    let len = x * x + y * y + z * z;
+    if (len > 0) {
+      //TODO: evaluate use of glm_invsqrt here?
+      len = 1 / Math.sqrt(len);
+    }
+    out[0] = a[0] * len;
+    out[1] = a[1] * len;
+    out[2] = a[2] * len;
+    return out;
+  }
+
+function cross(out, a, b) {
+    let ax = a[0],
+      ay = a[1],
+      az = a[2];
+    let bx = b[0],
+      by = b[1],
+      bz = b[2];
+    out[0] = ay * bz - az * by;
+    out[1] = az * bx - ax * bz;
+    out[2] = ax * by - ay * bx;
+    return out;
+  }
 
 function get_random_int(min, max) {
     min = Math.ceil(min);
@@ -351,6 +385,50 @@ function create_identity_4_matrix() {
     out[15] = 1;
     return out;
 }
+
+function invert(out, a) {
+    let a00 = a[0],
+      a01 = a[1],
+      a02 = a[2];
+    let a10 = a[3],
+      a11 = a[4],
+      a12 = a[5];
+    let a20 = a[6],
+      a21 = a[7],
+      a22 = a[8];
+    let b01 = a22 * a11 - a12 * a21;
+    let b11 = -a22 * a10 + a12 * a20;
+    let b21 = a21 * a10 - a11 * a20;
+    // Calculate the determinant
+    let det = a00 * b01 + a01 * b11 + a02 * b21;
+    if (!det) {
+      return null;
+    }
+    det = 1.0 / det;
+    out[0] = b01 * det;
+    out[1] = (-a22 * a01 + a02 * a21) * det;
+    out[2] = (a12 * a01 - a02 * a11) * det;
+    out[3] = b11 * det;
+    out[4] = (a22 * a00 - a02 * a20) * det;
+    out[5] = (-a12 * a00 + a02 * a10) * det;
+    out[6] = b21 * det;
+    out[7] = (-a21 * a00 + a01 * a20) * det;
+    out[8] = (a11 * a00 - a01 * a10) * det;
+    return out;
+  }
+
+function transformMat3(out, a, m) {
+    let x = a[0],
+        y = a[1],
+        z = a[2];
+    
+    out[0] = x * m[0] + y * m[3] + z * m[6];
+    out[1] = x * m[1] + y * m[4] + z * m[7];
+    out[2] = x * m[2] + y * m[5] + z * m[8];
+
+    return out;
+  }
+  
 
 function transformMat4(out, a, m) {
     let x = a[0],
